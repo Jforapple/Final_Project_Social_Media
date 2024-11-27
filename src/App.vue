790fa20ce86ce63c4f5bd62f9844d099b4fce57d
Jforapple <166318156+Jforapple@users.noreply.github.com>
2024-11-27 19:20:19 +0700

    1st Commit - Final Project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="text-2xl text-blue-500">Hello, Vite with Vue 3!</h1>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+/* Add any Tailwind CSS or custom styles here */
+</style>
